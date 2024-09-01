07fea547cc88653d6bafe3b7530ee6095f5a8135
lavabrando <1359241601@qq.com>
2024-09-01 22:46:01 +1000

    100

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class ConsolePrint : MonoBehaviour
 {
     // Start is called before the first frame update
-    private int i = 3;
     public Renderer rend;
+    private int i = 3;
 
     void Update()
     {
         i++;
         Debug.Log(gameObject.name + ":" + i);
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+        else if (gameObject.tag == "Blue" && i >= 150 && i <= 250)
+        {
+            int randomDisableTime = Random.Range(150, 251);
+            if (i == randomDisableTime)
+            {
+                rend.enabled = false;
+            }
+        }
     }
 
 }
